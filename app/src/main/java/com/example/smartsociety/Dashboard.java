77d32151c4930fdfdcd15090c8ed9cd979dec34c
Mahesh Dudhe <maheshdudhe0911@gmail.com>
2022-09-24 01:20:40 +0530

    This is Fourth(3) commit.Next working project.Added Parking management UI.

--- app/src/main/java/com/example/smartsociety/Dashboard.java
+++ app/src/main/java/com/example/smartsociety/Dashboard.java
@@ -2,13 +2,25 @@ package com.example.smartsociety;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
-
+    public Button parking;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        parking=findViewById(R.id.parkingbtn);
+        parking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Dashboard.this,parkingManagement.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
