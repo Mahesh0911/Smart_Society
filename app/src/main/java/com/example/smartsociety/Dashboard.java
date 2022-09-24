9629eda5250039c3ce6dcb04d0faed5a284a9264
Mahesh Dudhe <maheshdudhe0911@gmail.com>
2022-09-25 02:55:42 +0530

    This is Fourth(3.3) commit.Next working project.Added my vehicle,society,parking mngment,emergency alerts (UI only) .

--- app/src/main/java/com/example/smartsociety/Dashboard.java
+++ app/src/main/java/com/example/smartsociety/Dashboard.java
@@ -1,11 +1,20 @@
 package com.example.smartsociety;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
     public Button parking;
@@ -16,6 +25,8 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
         parking=findViewById(R.id.parkingbtn);
         vehiclebtn=findViewById(R.id.vehiclebtn);
         alertbtn=findViewById(R.id.alertbtn);
@@ -51,4 +62,5 @@ public class Dashboard extends AppCompatActivity {
             }
         });
     }
+
 }
