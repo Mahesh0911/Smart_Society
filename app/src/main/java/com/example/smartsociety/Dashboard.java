4d2e858a7158afd68fc3da2f9e26304831a5dd3e
Mahesh Dudhe <maheshdudhe0911@gmail.com>
2022-09-26 01:54:29 +0530

    This is Fifth(3.4) commit.Next working project.Added sidebar panel (notification setting,theme change,password,log out) (UI only) .

--- app/src/main/java/com/example/smartsociety/Dashboard.java
+++ app/src/main/java/com/example/smartsociety/Dashboard.java
@@ -3,34 +3,74 @@ package com.example.smartsociety;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class Dashboard extends AppCompatActivity {
     public Button parking;
     public Button vehiclebtn;
     public Button alertbtn;
     public Button societybtn;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-
         parking=findViewById(R.id.parkingbtn);
         vehiclebtn=findViewById(R.id.vehiclebtn);
         alertbtn=findViewById(R.id.alertbtn);
         societybtn=findViewById(R.id.societybtn);
+
+        drawerLayout=findViewById(R.id.drawerLayout);
+        navigationView=findViewById(R.id.navigation_view);
+
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.notificationSetting:
+                        Toast.makeText(Dashboard.this, "Notification Setting", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.changePass:
+                        Toast.makeText(Dashboard.this, "Change Password", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.uiTheme:
+                        Toast.makeText(Dashboard.this, "Change Theme", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.logout:
+                        Toast.makeText(Dashboard.this, "Log Out", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+
         vehiclebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
