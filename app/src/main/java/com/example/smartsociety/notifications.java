545381f7a33e1279db2d85464c824e7f8e13fa8a
A10-g <77630063+A10-g@users.noreply.github.com>
2022-09-25 12:47:23 +0530

    added notification activity

---
+++ app/src/main/java/com/example/smartsociety/notifications.java
@@ -0,0 +1,14 @@
+package com.example.smartsociety;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
